d5f033a355da813ddfc606f68aa1cb161d760a57
kinga00099 <kinga00099@gmail.com>
2019-04-09 10:59:30 +0200

    added stylus to webpack

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import "normalize.css"
 import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app');
