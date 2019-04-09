0b449c00b34fb5c8ba82a3b573f30410ce651de1
kinga00099 <kinga00099@gmail.com>
2019-04-09 14:28:52 +0200

    added vuetify theme

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import "normalize.css"
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import router from './router'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+    iconfont: 'md',
+    theme: {
+        primary: '#78909C',
+        accent: '#FFC107'
+    }
+});
 Vue.config.productionTip = false;
 
 new Vue({
