64665a189723cefd0310dd2e9a76a9a09db97305
kinga00099 <kinga00099@gmail.com>
2019-04-09 14:30:01 +0200

    prepared component for main page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import App from './App.vue'
+import Home from './components/Home.vue'
 
 Vue.use(Router);
 
@@ -9,7 +9,7 @@ export default new Router({
         {
             path: '/',
             name: 'Home',
-            component: App
+            component: Home
         }
     ],
     mode: 'history'
