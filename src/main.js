070c4d78aa5cff328dae2751220b90ba61eed21a
kinga00099 <kinga00099@gmail.com>
2019-04-10 14:21:42 +0200

    import axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import "normalize.css"
 import Vuetify from 'vuetify'
+
 import router from './router/index'
+import axios from 'axios'
+
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.headers.get['Accepts'] = 'application/json';
+
 
 Vue.use(Vuetify, {
     iconfont: 'md',
