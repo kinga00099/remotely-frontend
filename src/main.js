414d3aee470fbbd55a524c2edc82823ada1a24fe
kinga00099 <kinga00099@gmail.com>
2019-04-10 11:09:56 +0200

    restructure components

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import "normalize.css"
 import Vuetify from 'vuetify'
-import router from './router'
+import router from './router/index'
 
 Vue.use(Vuetify, {
     iconfont: 'md',
