c48b52c69423753254ad32bc71f4cc35c274be15
kinga00099 <kinga00099@gmail.com>
2019-04-11 10:25:21 +0200

    changed Navbar theme

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,7 @@
             </v-list>
         </v-navigation-drawer>
 
-        <v-toolbar dark class="primary">
+        <v-toolbar dark>
             <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up "></v-toolbar-side-icon>
             <v-toolbar-title>
                 <router-link to="/" tag="span" style="cursor: pointer" id="main-title">Remote.ly</router-link>
