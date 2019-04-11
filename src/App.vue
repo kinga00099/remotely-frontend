9281279a6f1ee29169c1a3e2a2186526b9696387
kinga00099 <kinga00099@gmail.com>
2019-04-11 10:24:47 +0200

    added searchbar to header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
     <v-app>
-        <navbar></navbar>
-        <header-component></header-component>
-        <filterbar></filterbar>
+        <div class="header__background">
+            <navbar></navbar>
+            <header-component></header-component>
+            <searchbar></searchbar>
+        </div>
         <v-content>
             <v-container>
                 <router-view></router-view>
@@ -15,7 +17,7 @@
 <script>
     import header from './components/Header.vue'
     import navbar from './components/Navbar.vue'
-    import filterbar from './components/Filterbar.vue'
+    import searchbar from './components/Searchbar.vue'
     import footer from './components/Footer.vue'
 
     export default {
@@ -24,11 +26,28 @@
             navbar,
             "footer-component": footer,
             "header-component": header,
-            filterbar
+            searchbar
         }
     }
 </script>
 
 <style lang="stylus">
     @import "assets/stylus/main.styl"
+
+    .header__background {
+        background: url("https://pixabay.com/get/e837b90f2df7093ed1534705fb0938c9bd22ffd41cb4154895f3c27da7/notebook-1280538_1920.jpg") left 5% bottom 98%/cover no-repeat border-box;
+    }
+
+
+    @media only screen and (min-width: 600px) and (max-width: 1264px) {
+        .header__background{
+            padding-bottom: 2rem;
+        }
+    }
+
+    @media only screen and (min-width: 1264px) {
+        .header__background {
+            padding-bottom: 4rem;
+        }
+    }
 </style>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-input>
+        <v-text-field class="search" v-model="search" label="Search" dark color="#fff" align-center :append-icon="icon">
+        </v-text-field>
+    </v-input>
+</template>
+
+<script>
+    export default {
+        name: "Search",
+        data() {
+            return {
+                icon: 'search',
+                search: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .search {
+        padding: 0 1rem;
+        margin: auto;
+        max-width: 80%;
+    }
+
+    @media only screen and (min-width: 600px) {
+        .search {
+            max-width: 50%;
+        }
+    }
+
+    @media only screen and (min-width: 1264px) {
+        .search {
+            max-width: 30%;
+        }
+    }
+</style>
