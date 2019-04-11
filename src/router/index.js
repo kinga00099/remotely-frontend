bc6d1902c5883cabec867c0d3b36568003e28841
kinga00099 <kinga00099@gmail.com>
2019-04-11 14:24:39 +0200

    non-existing routes now redirect to home page

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,10 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home
+        },
+        {
+            path: '*',
+            redirect: '/'
         }
     ],
     mode: 'history'
