88c17a5dfcfbfe73c0825d1d347c48a879a2d5ac
kinga00099 <kinga00099@gmail.com>
2019-05-08 16:23:43 +0200

    Add Job.vue to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home.vue'
+import Job from "../views/Job";
 
 Vue.use(Router);
 
@@ -14,6 +15,10 @@ export default new Router({
         {
             path: '*',
             redirect: '/'
+        },
+        {
+            path: '/job/:id',
+            component: Job
         }
     ],
     mode: 'history'
