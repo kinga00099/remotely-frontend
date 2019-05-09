773f4411b042f0ebb16eb7b6c2b16c9829b3045d
kinga00099 <kinga00099@gmail.com>
2019-05-09 10:27:46 +0200

    send access token with requests

---
+++ src/Api.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import Vue from 'vue'
+
+const SERVER_URL = 'http://localhost:5000/api/';
+
+const instance = axios.create({
+    baseURL: SERVER_URL,
+    timeout: 1000
+});
+
+export default {
+    async execute(method, resource, data, config) {
+        let accessToken = await Vue.prototype.$auth.getAccessToken();
+        return instance({
+            method:method,
+            url: resource,
+            data,
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            ...config
+        })
+    },
+
+    getAllJobs() {
+        return this.execute('GET','jobs')
+    },
+
+    getJobById(id){
+        return this.execute('GET', 'job/'+id)
+    }
+
+}
