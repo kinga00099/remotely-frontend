a99cc660532ae877d1176aae829f97289a118af1
kinga00099 <kinga00099@gmail.com>
2019-05-21 13:24:31 +0200

    refactor: login and logout functions moved here from App, change list rendering in template

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,11 +3,23 @@
 
         <v-navigation-drawer absolute temporary v-model="sideNav">
             <v-list>
-                <v-list-tile v-for="item in menuItems" :key="item.title" :to="item.link">
+                <v-list-tile :to="profile.link">
                     <v-list-tile-action>
-                        <v-icon>{{ item.icon }}</v-icon>
+                        <v-icon>{{ profile.icon }}</v-icon>
                     </v-list-tile-action>
-                    <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+                    <v-list-tile-content>{{ profile.title }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile v-if="isAuthenticated" v-on:click="logout()">
+                    <v-list-tile-action>
+                        <v-icon>{{ logoutElement.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>{{ logoutElement.title }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile v-if="!isAuthenticated" v-on:click="login()">
+                    <v-list-tile-action>
+                        <v-icon>{{ loginElement.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>{{ loginElement.title }}</v-list-tile-content>
                 </v-list-tile>
             </v-list>
         </v-navigation-drawer>
@@ -20,9 +32,17 @@
             <v-spacer></v-spacer>
 
             <v-toolbar-items class="hidden-xs-only">
-                <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.link">
-                    <v-icon left dark>{{ item.icon }}</v-icon>
-                    {{ item.title }}
+                <v-btn flat :to="profile.link">
+                    <v-icon left dark>{{ profile.icon }}</v-icon>
+                    {{ profile.title }}
+                </v-btn>
+                <v-btn flat v-if="isAuthenticated" v-on:click="logout()">
+                    <v-icon left dark>{{ logoutElement.icon }}</v-icon>
+                    {{ logoutElement.title }}
+                </v-btn>
+                <v-btn flat v-if="!isAuthenticated" v-on:click="login()">
+                    <v-icon left dark>{{ loginElement.icon }}</v-icon>
+                    {{ loginElement.title }}
                 </v-btn>
             </v-toolbar-items>
 
@@ -36,11 +56,27 @@
         data() {
             return {
                 sideNav: false,
-                menuItems: [
-                    {icon: 'person', title: 'Profile', link: '/profile'},
-                    {icon: 'face', title: 'Sign up', link: '/signup'},
-                    {icon: 'lock_open', title: 'Sign in', link: '/signin'}
-                ]
+                profile: {icon: 'person', title: 'Profile', link: '/profile'},
+                register: {icon: 'face', title: 'Sign up', link: '/signup'},
+                loginElement: {icon: 'lock_open', title: 'Sign in', link: '/login'},
+                logoutElement: {icon: 'exit_to_app', title: 'Logout'}
+            }
+        },
+        methods: {
+            login() {
+                this.$auth.loginRedirect('/')
+            },
+            async logout() {
+                await this.$auth.logout();
+                await this.$store.dispatch('checkIfAuthenticated', this.$auth);
+                this.$router.replace('/')
+            }
+        },
+        computed: {
+            isAuthenticated() {
+                const auth = this.$store.getters.getAuthStatus;
+                this.$log.info('authenticated navbar:', auth);
+                return auth
             }
         }
     }
@@ -49,13 +85,13 @@
 <style lang="stylus" scoped>
     @import url('https://fonts.googleapis.com/css?family=Damion');
 
-    .v-btn:hover, .v-btn__content:hover{
+    .v-btn:hover, .v-btn__content:hover {
         color: black;
         background-color: #FFC107
     }
 
     #main-title {
         font-family: 'Damion', cursive;
-        font-size : 3rem;
+        font-size: 3rem;
     }
 </style>
